8da26f60354476a962e3379e0dcdccc664ea3afc
George <sonicgd@gmail.com>
2019-03-13 11:20:26 +0500

    Rework modules to use without host builder

--- src/BioEngine.Extra.Twitter/TwitterModule.cs
+++ src/BioEngine.Extra.Twitter/TwitterModule.cs
@@ -4,20 +4,22 @@ using BioEngine.Core.Modules;
 using BioEngine.Core.Properties;
 using BioEngine.Extra.Twitter.Service;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BioEngine.Extra.Twitter
 {
     public class TwitterModule : BioEngineModule
     {
-        public override void ConfigureServices(WebHostBuilderContext builderContext, IServiceCollection services)
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration,
+            IHostingEnvironment environment)
         {
-            services.Configure<TwitterServiceConfiguration>(configuration =>
+            services.Configure<TwitterServiceConfiguration>(config =>
             {
-                configuration.ConsumerKey = builderContext.Configuration["BE_TWITTER_CONSUMER_KEY"];
-                configuration.ConsumerSecret = builderContext.Configuration["BE_TWITTER_CONSUMER_SECRET"];
-                configuration.AccessToken = builderContext.Configuration["BE_TWITTER_ACCESS_TOKEN"];
-                configuration.AccessTokenSecret = builderContext.Configuration["BE_TWITTER_ACCESS_TOKEN_SECRET"];
+                config.ConsumerKey = configuration["BE_TWITTER_CONSUMER_KEY"];
+                config.ConsumerSecret = configuration["BE_TWITTER_CONSUMER_SECRET"];
+                config.AccessToken = configuration["BE_TWITTER_ACCESS_TOKEN"];
+                config.AccessTokenSecret = configuration["BE_TWITTER_ACCESS_TOKEN_SECRET"];
             });
             services.AddSingleton<TwitterService>();
             services.AddScoped<IRepositoryFilter, TwitterContentFilter>();
